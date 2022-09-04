edb78a6468edb61314c8a2fbdfd8485f93d9fff7
Дмитрий Карманов <karmanovds@gmail.com>
2022-09-04 21:26:00 +0700

    added file final-work.cs

---
+++ final-work.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+
+namespace Exercise
+{
+    class Program
+	{
+		static void Main(string[] args)
+		{	
+ 			void Exercise()
+			{
+
+            }
+        
+        //void Exercise();
+        {
+    {
+{
