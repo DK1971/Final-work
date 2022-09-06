23fb5641248bf2d65d9afdfd41de569b59477794
Дмитрий Карманов <karmanovds@gmail.com>
2022-09-06 23:36:01 +0700

    added array1 and array2

--- final-work.cs
+++ final-work.cs
@@ -9,7 +9,9 @@ namespace Exercise
 		{	
  			void Exercise()
 			{
-
+                Console.WriteLine("Задан первоначальный массив из строк:" );
+				string[] array1 = { "123", "zero", "one", "2", "two", "three", ":-)", "four"};
+				string[] array2 = new string [array1.Length];
             }
         
         //void Exercise();
