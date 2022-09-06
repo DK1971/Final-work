6d5519b4871d25fb36fa64215f7913198180e7a9
Дмитрий Карманов <karmanovds@gmail.com>
2022-09-06 23:37:10 +0700

    added void PrintArray

--- final-work.cs
+++ final-work.cs
@@ -12,6 +12,15 @@ namespace Exercise
                 Console.WriteLine("Задан первоначальный массив из строк:" );
 				string[] array1 = { "123", "zero", "one", "2", "two", "three", ":-)", "four"};
 				string[] array2 = new string [array1.Length];
+
+                void PrintArray (string[]array)
+				{
+					for (int i = 0; i < array.Length; i++)
+					{
+						Console.Write(array[i] + " ");
+					}
+					Console.WriteLine();
+				}		
             }
         
         //void Exercise();
